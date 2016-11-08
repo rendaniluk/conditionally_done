8bf2cbb92636e0fe14bc046a1d08cf37d696cd14
rendaniluk <lukhwa11582924@gmail.com>
2016-11-08 11:06:01 +0200

    first commit

---
+++ if_tuesday.js
@@ -0,0 +1,13 @@
+//storing Monday on day variable
+var day = 'Monday';
+//setting day equal to Tuesday and store it on isTuesday
+var isTuesday = day ==='Tuesday';
+//using conditional statement to check if is Tuesday
+if (isTuesday){
+  //if day is equal to Tuesday excute this
+  console.log('Hey, hey it is Tuesday!');
+}
+else{
+  //if day is not equal to Tuesday excute this
+  console.log('Some other day : ' + day);
+}
