3f736e7da667ece3347bc8b36864005543bd0b12
rendaniluk <lukhwa11582924@gmail.com>
2016-11-09 18:39:20 +0200

    initial commit

---
+++ weekOrWeekend.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+//declaring the function
+var weekOrWeekend = function(day){
+//condition for weekdays
+  var week = !day.startsWith('S');
+//conditional statement.
+    if (week){return 'week'
+    }else{
+      return 'weekend'
+     }
+};
+//calling function and store it on varibles
+var days = weekOrWeekend('Wednesday');
+console.log(days);
+var today = weekOrWeekend('Sunday');
+console.log(today);
+//testing using assert functions
+assert.equal(weekOrWeekend('Wednesday'),'week');
+assert.equal(weekOrWeekend('Sunday'),'weekend');
